e346028e0562ff857aca5a814f02a4d31b8accc8
enderpicture <enderpicture@gmail.com>
2021-12-11 00:37:09 -0800

    finished page transitions

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -1,24 +1,9 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 	import { cubicInOut } from 'svelte/easing';
-
-	function spin(node: HTMLElement, { duration }: { duration: number }) {
-		return {
-			duration,
-			css: (t: number) => {
-				const eased = cubicInOut(t);
-
-				return `
-          opacity: ${eased};
-          // height: ${node.clientHeight};
-					// transform: translateX(${(1 - eased) * 100}px);
-					`;
-			}
-		};
-	}
 </script>
 
-<main transition:fade={{ duration: 10000 }}>
+<main transition:fade={{ duration: 50 }}>
 	<slot />
 </main>
 
