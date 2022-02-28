4a78eaffb62c77d05bc50542eef0c721a5bc63aa
enderpicture <enderpicture@gmail.com>
2022-02-27 21:53:54 -0800

    stay on last selected location

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,14 +3,18 @@ import type { WeatherLocation } from '$lib/types/types';
 import { browser } from '$app/env';
 
 export const weatherLocations = writable<WeatherLocation[]>([]);
+export const selectedLocation = writable<number>(-1);
 
 if (browser) {
 	weatherLocations.set(
-		JSON.parse(localStorage.getItem('weatherLocations') || '[]') as WeatherLocation[]
+		JSON.parse(localStorage.getItem('weatherLocations') ?? '[]') as WeatherLocation[]
 	);
 	weatherLocations.subscribe((value) => {
 		localStorage.setItem('weatherLocations', JSON.stringify(value));
 	});
-}
 
-export const selectedLocation = writable<number>(-1);
+	selectedLocation.set(Number.parseInt(localStorage.getItem('selectedLocation') ?? '-1'));
+	selectedLocation.subscribe((value) => {
+		localStorage.setItem('selectedLocation', value.toString());
+	});
+}
